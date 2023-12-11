25b30cde24fae7665cc3113d875240f28d7a2f41
Shourov00 <sfazleyrabbi@gmail.com>
2023-12-11 12:37:13 +0600

    database crud operations

--- blog/database.py
+++ blog/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog.db'
 
 # check_same_thread ...is needed only for SQLite. It's not needed for other databases.
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread', False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- blog/main.py
+++ blog/main.py
@@ -1,9 +1,65 @@
-from fastapi import FastAPI
-from .schemas import Blog
+from fastapi import FastAPI, Depends, status, Response, HTTPException
+from sqlalchemy.orm import Session
+
+from . import schemas, models
+from .database import engine, SessionLocal
 
 app = FastAPI()
 
+# create all tables
+models.Base.metadata.create_all(bind=engine)
 
-@app.post('/blog')
-def create(blog: Blog):
-    return {'title': blog.title, 'body': blog.body}
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+@app.get('/blog')
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get('/blog/{id}')
+def show(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} is not available')
+
+    return blog
+
+
+@app.delete('/blog/{id}')
+def destroy(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} is not available')
+
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} is not available')
+
+    blog.update(request.model_dump())
+    db.commit()
+    return 'updated'
