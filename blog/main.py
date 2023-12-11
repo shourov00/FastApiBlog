04a198eca8f4e3c9307b68dc4a00340090af5768
Shourov00 <sfazleyrabbi@gmail.com>
2023-12-11 22:44:41 +0600

    user model

--- blog/main.py
+++ blog/main.py
@@ -1,8 +1,12 @@
+from typing import List
+
 from fastapi import FastAPI, Depends, status, Response, HTTPException
 from sqlalchemy.orm import Session
+from passlib.context import CryptContext
 
 from . import schemas, models
 from .database import engine, SessionLocal
+from .hashing import Hash
 
 app = FastAPI()
 
@@ -28,13 +32,13 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get('/blog')
+@app.get('/blog', response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}')
+@app.get('/blog/{id}', response_model=schemas.ShowBlog)
 def show(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -63,3 +67,21 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     blog.update(request.model_dump())
     db.commit()
     return 'updated'
+
+
+@app.post('/user', response_model=schemas.ShowUser)
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@app.get('/user/{id}', response_model=schemas.ShowUser)
+def get_user(id, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with id {id} not found')
+
+    return user
--- blog/schemas.py
+++ blog/schemas.py
@@ -4,3 +4,18 @@ from pydantic import BaseModel
 class Blog(BaseModel):
     title: str
     body: str
+
+
+class ShowBlog(BaseModel):
+    title: str
+
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
