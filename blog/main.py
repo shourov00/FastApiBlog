2041299ef4c39f319683c47681be32df957e3247
Shourov00 <sfazleyrabbi@gmail.com>
2023-12-11 10:21:40 +0600

    database connection

---
+++ blog/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from .schemas import Blog
+
+app = FastAPI()
+
+
+@app.post('/blog')
+def create(blog: Blog):
+    return {'title': blog.title, 'body': blog.body}
--- main.py
+++ main.py
@@ -1,13 +1,46 @@
+from typing import Optional
+
+import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {'data': {'name': 'Rabbi'}}
+@app.get('/blog')
+def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blog list'}
+    else:
+        return {'data': f'{limit} blog list'}
 
 
-@app.get('/about')
-def about():
-    return {'data': {'name': 'Abbout Page'}}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog with id
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10):
+    return {'data': {'1', '2'}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data': f'Blog is created with title as {blog.title}'}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
